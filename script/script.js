d6c91f76e3b49a10a7a55de6d2016ae888f150e7
raonircl <raoni.afi@gmail.com>
2021-08-13 19:16:06 -0300

    atualização de script

--- script/script.js
+++ script/script.js
@@ -1,45 +1,45 @@
-'use strict'
 
-document.getElementById("formulario").addEventListener("submit". cadastrarVeiculo)
+document.getElementById('formulario').addEventListener('submit', cadastrarVeiculo)
 
 function cadastrarVeiculo(e){
 
-    const modeloVeiculo = document.getElementById("modeloVeiculo").value;
-    const placaVeiculo = document.getElementById("placaVeiculo").value;
-    const horaEntrada = new Date();
+    var modeloVeiculo = document.getElementById('modeloVeiculo').value;
+    var placaVeiculo = document.getElementById('placaVeiculo').value;
+    var horaEntrada = new Date();
 
     if(!modeloVeiculo && !placaVeiculo){
 
-        alert("Preencha todos os campos!");
+        alert('Preencha todos os campos!');
         return false;
     }
 
-    const veiculo = {
+    var veiculo = {
         modelo: modeloVeiculo,
         placa: placaVeiculo,
         hora: horaEntrada.getHours(),
         minutos: horaEntrada.getMinutes()
     };
 
-    if(localStorage.getItem("patio") === null){
-        const veiculos = [];
+    if(localStorage.getItem('patio') === null){
+        var veiculos = [];
         veiculos.push(veiculo);
-        localStorage.setItem("patio", JSON.stringify(veiculos));
-    }else{
-        const veiculos = JSON.parse(localStorage.getItem("patio"));
+        localStorage.setItem('patio', JSON.stringify(veiculos));
+    }else {
+        var veiculos = JSON.parse(localStorage.getItem('patio'));
         veiculos.push(veiculo);
-        localStorage.setItem("patio", JSON.stringify(veiculos));
+        localStorage.setItem('patio', JSON.stringify(veiculos));
     }
 
-    document.getElementById("formulario").reset();
+    document.getElementById('formulario').reset();
 
     mostraPatio();
+
     e.preventDefault();
 
 }
 
 function removeVeiculo(placa){
-    var patio = JSON.parse(localStorage.getItem("patio"));
+    var patio = JSON.parse(localStorage.getItem('patio'));
     console.log(patio);
 
         for( var i = 0; i < patio.length; i++){
@@ -48,24 +48,25 @@ function removeVeiculo(placa){
             }
 
         }
-        localStorage.setItem("patio", JSON.stringify(patio));
+        localStorage.setItem('patio', JSON.stringify(patio));
+        
         mostraPatio();
 }
 function mostraPatio(){
-    const veiculos = JSON.parse(localStorage.getItem("patio"));
-    const patioResultado = document.getElementById("resultados");
+    var veiculos = JSON.parse(localStorage.getItem('patio'));
+    var patioResultado = document.getElementById('resultados');
 
-    patioResultado.innerHTML = "";
+    patioResultado.innerHTML = '';
 
-    for( const i = 0; i < veiculos.length; i++){
-        const modelo = veiculos[i].modelo;
-        const placa = veiculos[i].placa;
-        const hora = veiculos[i].hora;
-        const minutos = veiculos[i].minutos;
+    for( var i = 0; i < veiculos.length; i++){
+        var modelo = veiculos[i].modelo;
+        var placa = veiculos[i].placa;
+        var hora = veiculos[i].hora;
+        var minutos = veiculos[i].minutos;
             patioResultado.innerHTML += '<tr><td>'+ modelo + '</td>'+
             '<td>'+ placa + '</td>' +
             '<td>'+ hora + ':' + minutos + '</td>' +
             '<td><button onclick="removeVeiculo(\''+ placa +'\')" class="btn btn-danger">Remover</button></td>'+
-      '</tr>'; 
-    }
+      '</tr>';
+}
 }
